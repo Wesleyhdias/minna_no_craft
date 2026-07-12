d015a88764003d501701e1f49ba8cb5239e0475e
Wesleyhdias <wesleyhdmelo@gmail.com>
2026-07-12 20:02:32 -0300

    initial commit

---
+++ src/main/java/com/wesleyhdias/minnanocraft/MinnaNoCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wesleyhdias.minnanocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinnaNoCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
